003720a9a7176ad89f841308e13351686e8d5430
trondil <Trond.Inge.Lillesand@nav.no>
2019-10-26 16:55:11 +0200

    A case of edit distance using Levensthein

---
+++ dpPractice/levensteinish.cpp
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+
+using namespace  std;
+
+    int levensteinish(string word1, string word2) {
+        
+        int m = word1.length() + 1;
+        int n = word2.length() + 1; 
+
+        int **grid = new int*[m];
+        for(int x = 0; x < m; ++x) {
+            grid[x] = new int[n];
+        }
+        // Fill xborder
+        for (int x = 0; x < m; ++x) {
+            grid[x][0] = x;
+        }
+        // Fill yborder
+        for (int y = 0; y < n; ++y){
+            grid[0][y] = y;
+        }
+        // Trace 
+        for (int x = 1; x < m; ++x) {
+            for (int y = 1; y < n; ++y) {         
+                if (word1.at(x-1) == word2.at(y-1)) {
+                    grid[x][y] = grid[x-1][y-1]; 
+                }
+                else {
+                    grid[x][y] = min(min(grid[x-1][y], grid[x][y-1]), grid[x-1][y-1]) + 1;
+                }
+            }
+        }
+                for (int x = 0; x < m; ++x) {
+                    cout << endl; 
+            for (int y = 0; y < n; ++y) {         
+                cout << grid[x][y] << " " ; 
+            }
+        }
+     
+        return grid[m-1][n-1]; 
+    }
+
+int main(){
+
+    //
+    cout << levensteinish("meilenstein", "levenshtein") << endl; 
+}
